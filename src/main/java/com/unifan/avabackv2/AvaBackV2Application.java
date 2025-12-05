2bed4fa5f28fd45371a8930dc98b31e3eee745ea
gaelhbs <gabrielsouza1170@gmail.com>
2025-12-05 18:58:07 -0300

    Initial commit

---
+++ src/main/java/com/unifan/avabackv2/AvaBackV2Application.java
@@ -0,0 +1,13 @@
+package com.unifan.avabackv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaBackV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaBackV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/unifan/avabackv2/AvaBackV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifan.avabackv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaBackV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
